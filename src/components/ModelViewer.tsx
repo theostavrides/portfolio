44806e978ee201ed85ec13d28c356d97fc098ae6
Theo Stavrides <theostavrides@gmail.com>
2023-07-14 15:24:54 -0700

    fix lighting in model viewer

--- src/components/ModelViewer.tsx
+++ src/components/ModelViewer.tsx
@@ -5,8 +5,8 @@ import { useEffect } from 'react';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
 const canvasParams = {
-    width: 300,
-    height: 300,
+    width: 500,
+    height: 720,
 }
 
 interface IParams {
@@ -21,21 +21,24 @@ export default function ModelViewer ({ id }: IParams) {
             const scene = new THREE.Scene()
     
             const setupModel = (data: any) => {
-                const model = data.scene.children[0]
-              
-                return model
+                return data.scene.children[0]
             }
               
             const loadModels = async () => {
                 const loader = new GLTFLoader();
-                const modelData = await loader.loadAsync('models/building.gltf')
-                const building = setupModel(modelData)
+                const modelData = await loader.loadAsync('models/head.glb')
+                const model = setupModel(modelData) as THREE.Object3D
+
+                model.scale.x = 70
+                model.scale.y = 70
+                model.scale.z = 70
     
-                return { building }
+                return { model }
             }
     
-            const camera = new THREE.PerspectiveCamera(50, 1, 1, 1000)
+            const camera = new THREE.PerspectiveCamera(50, canvasParams.width / canvasParams.height, 1, 1000)
             camera.position.z = 35
+            camera.position.y = 3
     
             const canvas = document.getElementById(canvasId) as HTMLCanvasElement
     
@@ -45,29 +48,33 @@ export default function ModelViewer ({ id }: IParams) {
                 antialias: true,
                 // alpha: true
             })
-            renderer.setClearColor( 0x000000, 0 );
-    
-            const ambientLight = new THREE.AmbientLight(0xffffff, 0.1)
-            ambientLight.castShadow = true
-            scene.add(ambientLight)
 
-            const light = new THREE.PointLight( 0xff0000, 20, 100 );
-            light.position.set( 50, 50, 50 );
+            // renderer.setClearColor( 0x000000, 0 );
+            // 
+    
+            // const ambientLight = new THREE.AmbientLight(0xffffff, .2)
+            // ambientLight.castShadow = true
+            // scene.add(ambientLight)
+
+            const light = new THREE.PointLight( 0xffffff, 1, 1000 );
+            light.position.set( -20, 20, 10 );
             scene.add( light );
+
+            const light2 = new THREE.PointLight( 0xeeeeee, 1, 1000 );
+            light2.position.set( 10, 10, 40 );
+            scene.add( light2 );
     
             renderer.setSize(canvasParams.width, canvasParams.height)
     
             const models = await loadModels()
             
-            scene.add(models.building)
-    
-    
-    
-    
+            scene.add(models.model)
+        
             const animate = () => {
-                models.building.rotation.x += .005
-                models.building.rotation.y += .005
-                renderer.render(scene, camera)
+                models.model.rotation.z = 0.25 * Math.cos(Date.now()/1000)
+                models.model.rotation.y = 0.25 * Math.cos(Date.now()/10000) + (-.05 * Math.PI)
+                renderer.render(scene, camera) 
+                console.log('r')
                 window.requestAnimationFrame(animate)
             }
     
